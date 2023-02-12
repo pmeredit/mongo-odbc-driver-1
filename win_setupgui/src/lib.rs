41e06c448e23e17e2e4ba87f0c930bc973af1140
Patrick Meredith <pmeredit@gmail.com>
2023-02-12 16:37:03 -0500

    SETUPGUI: Test reason

--- win_setupgui/src/lib.rs
+++ win_setupgui/src/lib.rs
@@ -1,28 +1,42 @@
 //#[cfg(target_os = "windows")]
 use windows::{
     core::*,
-    Win32::{Foundation::HINSTANCE, UI::WindowsAndMessaging::*},
+    Win32::{
+        Foundation::HINSTANCE,
+        System::SystemServices::{DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH},
+        UI::WindowsAndMessaging::*,
+    },
 };
 
 //#[cfg(target_os = "windows")]
 #[no_mangle]
-pub extern "system" fn DllMain(_: HINSTANCE, _: u32, _: usize) -> i32 {
+pub extern "system" fn DllMain(_: HINSTANCE, reason_for_call: u32, _: usize) -> i32 {
     unsafe {
-        MessageBoxW(None, w!("FFF"), w!("SSSS"), MB_OK);
-        CreateWindowExW(
-            WS_EX_LAYERED,
-            w!("AtlasSQL ODBC Data Source Setup"),
-            w!("FOO"),
-            WS_OVERLAPPEDWINDOW,
-            200,
-            200,
-            400,
-            300,
-            None,
-            None,
-            None,
-            None,
-        );
+        match reason_for_call {
+            DLL_PROCESS_ATTACH => {
+                MessageBoxW(None, w!("ATTACH"), w!("ATTACHS"), MB_OK);
+            }
+            DLL_PROCESS_DETACH => {
+                MessageBoxW(None, w!("DETACH"), w!("DETACHS"), MB_OK);
+            }
+            _ => {
+                MessageBoxW(None, w!("UNKNOWN"), w!("UNKNOWN"), MB_OK);
+            }
+        }
+        //        CreateWindowExW(
+        //            WS_EX_LAYERED,
+        //            w!("AtlasSQL ODBC Data Source Setup"),
+        //            w!("FOO"),
+        //            WS_OVERLAPPEDWINDOW,
+        //            200,
+        //            200,
+        //            400,
+        //            300,
+        //            None,
+        //            None,
+        //            None,
+        //            None,
+        //        );
     }
     1
 }
