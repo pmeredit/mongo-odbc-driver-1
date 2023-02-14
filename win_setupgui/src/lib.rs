f3758abbaef84d4790b9bc93b821327289d178af
Patrick Meredith <pmeredit@gmail.com>
2023-02-14 15:25:28 -0500

    SETUPGUID: Test out native-windows-gui lib

--- win_setupgui/src/lib.rs
+++ win_setupgui/src/lib.rs
@@ -1,3 +1,9 @@
+#![windows_subsystem = "windows"]
+/*!
+    A very simple application that shows your name in a message box.
+    Unlike `basic_d`, this example uses layout to position the controls in the window
+*/
+
 //#[cfg(target_os = "windows")]
 use widechar::to_widechar_vec;
 use windows::{
@@ -8,6 +14,57 @@ use windows::{
         UI::WindowsAndMessaging::*,
     },
 };
+
+extern crate native_windows_derive as nwd;
+extern crate native_windows_gui as nwg;
+
+use nwd::NwgUi;
+use nwg::NativeUi;
+
+#[derive(Default, NwgUi)]
+pub struct BasicApp {
+    #[nwg_control(size: (300, 115), position: (300, 300), title: "Basic example", flags: "WINDOW|VISIBLE")]
+    #[nwg_events( OnWindowClose: [BasicApp::say_goodbye] )]
+    window: nwg::Window,
+
+    #[nwg_layout(parent: window, spacing: 1)]
+    grid: nwg::GridLayout,
+
+    #[nwg_control(text: "Heisenberg", focus: true)]
+    #[nwg_layout_item(layout: grid, row: 0, col: 0)]
+    name_edit: nwg::TextInput,
+
+    #[nwg_control(text: "Say my name")]
+    #[nwg_layout_item(layout: grid, col: 0, row: 1, row_span: 2)]
+    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
+    hello_button: nwg::Button,
+}
+
+impl BasicApp {
+    fn say_hello(&self) {
+        nwg::modal_info_message(
+            &self.window,
+            "Hello",
+            &format!("Hello {}", self.name_edit.text()),
+        );
+    }
+
+    fn say_goodbye(&self) {
+        nwg::modal_info_message(
+            &self.window,
+            "Goodbye",
+            &format!("Goodbye {}", self.name_edit.text()),
+        );
+        nwg::stop_thread_dispatch();
+    }
+}
+
+fn init_gui() {
+    nwg::init().expect("Failed to init Native Windows GUI");
+    nwg::Font::set_global_family("Segoe UI").expect("Failed to set default font");
+    let _app = BasicApp::build_ui(Default::default()).expect("Failed to build UI");
+    nwg::dispatch_thread_events();
+}
 
 //#[cfg(target_os = "windows")]
 #[no_mangle]
@@ -55,6 +112,7 @@ pub extern "system" fn ConfigDSNW(
         let o = to_widechar_vec(&request.to_string());
         let o = PCWSTR::from_raw(o.as_ptr());
         MessageBoxW(None, o, w!("REQUEST"), MB_OK);
+        init_gui();
     }
     true
 }
