8d5d42ee82cfbb2640370a2d7f71fbcf3775eb4e
Patrick Meredith <pmeredit@gmail.com>
2023-02-14 19:33:32 -0500

    SETUPGUID: Layout testing

--- win_setupgui/src/lib.rs
+++ win_setupgui/src/lib.rs
@@ -38,15 +38,15 @@ pub struct BasicApp {
     //#[nwg_layout_item(layout: grid, row: 0, col: 0)]
     //frame: nwg::ImageFrame,
     #[nwg_control(text: "", focus: true)]
-    #[nwg_layout_item(layout: grid, row: 1, col: 0)]
+    #[nwg_layout_item(layout: grid, row: 0, col: 0)]
     driver_name: nwg::TextInput,
 
     #[nwg_control(text: "Description", focus: false)]
-    #[nwg_layout_item(layout: grid, row: 1, col: 1)]
+    #[nwg_layout_item(layout: grid, row: 0, col: 1)]
     description: nwg::TextInput,
 
     #[nwg_control(text: "Say my name")]
-    #[nwg_layout_item(layout: grid, row: 2, col: 0, row_span: 2, col_span: 2)]
+    #[nwg_layout_item(layout: grid, row: 1, col: 0, row_span: 2, col_span: 2)]
     #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
     hello_button: nwg::Button,
 }
