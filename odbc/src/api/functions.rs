1845e7a8ff10817413669fb4f6ad16176ed6db72
Patrick Meredith <pmeredit@gmail.com>
2023-06-12 15:50:07 -0400

    Update comment

--- odbc/src/api/functions.rs
+++ odbc/src/api/functions.rs
@@ -2044,7 +2044,9 @@ macro_rules! sql_get_info_helper {
                 | InfoType::SQL_CONVERT_VARBINARY
                 | InfoType::SQL_CONVERT_LONGVARBINARY
                 | InfoType::SQL_CONVERT_GUID => {
-                    // MongoSQL does not support CONVERT.
+                    // MongoSQL does not support the CONVERT scalar function, but clients also use
+                    // this to apply the CAST syntactic construct. The value we return for
+                    // SQL_CONVERT_FUNCTIONS alerts the client that we expect CAST and not CONVERT.
                     i16_len::set_output_fixed_data(&SQL_U32_ALL, info_value_ptr, string_length_ptr)
                 }
                 InfoType::SQL_GETDATA_EXTENSIONS => {
